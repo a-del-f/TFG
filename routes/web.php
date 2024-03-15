be8492e868da39bd502b297e0907828ccf809a98
Alejandro <adelfresnomartin@gmail.com>
2024-03-15 14:17:44 +0100

    update 15/03

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
+
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [AuthController::class, 'index'])->name('home');
+Route::post('/custom-login', [AuthController::class, 'login'])->name('login');
+Route::get('/logados', [AuthController::class, 'logados'])->name('logados');
