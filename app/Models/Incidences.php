519c93c25ced519a65bbbc63e155d4aa79140f3b
Alejandro <adelfresnomartin@gmail.com>
2024-04-04 21:43:23 +0200

    update 04/04(2)

--- app/Models/Incidences.php
+++ app/Models/Incidences.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 // Importa la clase Model
 
-class Incidences extends Authenticatable // Extiende la clase Model
+class Incidences extends Model // Extiende la clase Model
 {
     use HasFactory, Notifiable;
     public $timestamps = false;
