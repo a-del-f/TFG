6a7f5def31af050e89698f964780850d1d0e7a34
a-del-f <adelfresnomartin@gmail.com>
2024-04-25 22:40:18 +0200

    update 25/04

---
+++ database/migrations/create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("messages", function (Blueprint $table){
+            $table->id();
+            $table->bigInteger("id_incidence")->nullable()->unsigned();
+            $table->bigInteger("id_department")->nullable()->unsigned();
+            $table->bigInteger("id_aula")->nullable()->unsigned();
+            $table->string("description")->nullable();
+            $table->dateTime('fecha_creacion')->default(now());
+            $table->enum('estado', ['en espera', 'solucionando', 'solucionado'])->default('en espera');
+            $table->foreign("id_incidence")->references("id")->on("incidences");
+            $table->foreign("id_department")->references("id")->on("department");
+            $table->foreign("id_aula")->references("id")->on("aula");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+
+    }
+};
